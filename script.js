efd5e4dc6e893d8428bcbd8fb416d2dfdf5cbb41
VadymMakohon <errorks7@gmail.com>
2024-09-26 11:00:59 +0200

    added functions

--- script.js
+++ script.js
@@ -30,3 +30,47 @@ resetBtn.addEventListener("click", () => {
     reset()
     startBtn.innerHTML = "Start"
 })
+function start() {
+    if (timeBegan === null)
+        timeBegan = new Date()
+
+    if (timeStopped !== null)
+        stoppedDuration += (new Date() - timeStopped)
+    // setInterval runs the timeRunning function continuously
+    // startInterval in needed to clear the interval
+    startInterval = setInterval(timeRunning)
+}
+
+function reset() {
+    clearInterval(startInterval)
+    timeBegan = null
+    timeStopped = null
+    stoppedDuration = 0
+    flag = false
+    hour.innerHTML = "00"
+    minute.innerHTML = "00"
+    second.innerHTML = "00"
+    millisecond.innerHTML = "00"
+}
+
+function stop() {
+    timeStopped = new Date()
+    clearInterval(startInterval)
+}
+
+function timeRunning() {
+    const currentTime = new Date()
+    const timeElapsed = new Date(currentTime - timeBegan - stoppedDuration)
+
+    let hours = timeElapsed.getUTCHours()
+    let minutes = timeElapsed.getUTCMinutes()
+    let seconds = timeElapsed.getUTCSeconds()
+    let milliseconds = timeElapsed.getUTCMilliseconds()
+
+    milliseconds = Math.floor(milliseconds / 10)
+
+    hour.innerHTML = (hours = hours < 10 ? '0' + hours : hours)
+    minute.innerHTML = (minutes = minutes < 10 ? '0' + minutes : minutes)
+    second.innerHTML = (seconds = seconds < 10 ? '0' + seconds : seconds)
+    millisecond.innerHTML = (milliseconds = milliseconds < 10 ? '0' + milliseconds : milliseconds)
+}
